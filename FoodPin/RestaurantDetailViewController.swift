cae0a7ee7cddd4de66ff2433f185f0f27c311a17
Jan Hovland <janhovland@jans-imac.local>
2018-09-10 10:23:52 +0200

    Initial Commit

---
+++ FoodPin/RestaurantDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RestaurantDetailViewController.swift
+//  FoodPin
+//
+//  Created by Jan Hovland on 08/09/2018.
+//  Copyright © 2018 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailViewController: UIViewController {
+
+    @IBOutlet var restaurantImageView: UIImageView!
+    @IBOutlet var restaurantNameLabel: UILabel!
+    @IBOutlet var restaurantTypeLabel: UILabel!
+    @IBOutlet var restaurantLocationLabel: UILabel!
+    
+    var restaurantImageName = ""
+    var restaurantName = ""
+    var restaurantType = ""
+    var restaurantLocation = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        restaurantImageView.image = UIImage(named: restaurantImageName)
+        restaurantNameLabel.text = restaurantName
+        restaurantTypeLabel.text = restaurantType
+        restaurantLocationLabel.text = restaurantLocation
+        
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+}
